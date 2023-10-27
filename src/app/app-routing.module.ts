de84e194e6355821c34ca8ee2df006a749cde11a
itaji-create <itaji.1999@gmail.com>
2023-10-26 21:49:12 -0300

    cria componente page Products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products', component: ProductsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
